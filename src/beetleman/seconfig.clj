83917bfb674b3efb315baecf279990d311603baa
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2020-04-19 20:30:56 +0200

    implement support for nested maps and ignoring not supported data types

--- src/beetleman/seconfig.clj
+++ src/beetleman/seconfig.clj
@@ -2,8 +2,26 @@
   (:require [caesium.randombytes :as r]
             [caesium.crypto.box :as cb]
             [caesium.crypto.secretbox :as sb]
-            [clojure.walk :as walk])
+            [clojure.edn :as edn]
+            [clojure.walk :as walk]
+            [clojure.tools.logging :as log]
+            [clojure.string :as string])
   (:import (org.apache.commons.codec.binary Hex)))
+
+
+(def prefix (str ::encrypted ":"))
+
+
+(defn- -with-prefix
+  [value]
+  (str prefix value))
+
+
+(def -trim-prefix-re (re-pattern (str "^" prefix)))
+
+(defn- -trim-prefix
+  [value]
+  (string/replace-first value -trim-prefix-re ""))
 
 
 (defn keypair!
@@ -20,45 +38,70 @@
   (Hex/encodeHexString b))
 
 
+(defn- -encrypt-it?
+  [value]
+  (or (number? value)
+      (string? value)))
+
+
+(defn- -decrypt-it?
+  [value]
+  (and (string? value)
+       (string/starts-with? value prefix)))
+
 
 (defn- -encrypt-value
   [user-secret-key ci-public-key value]
-  (bytes->hex (cb/encrypt ci-public-key
-                          user-secret-key
-                          (sb/int->nonce 0)
-                          (.getBytes value))))
+  (if (-encrypt-it? value)
+    (->> value
+         pr-str
+         (.getBytes)
+         (cb/encrypt ci-public-key
+                     user-secret-key
+                     (sb/int->nonce 0))
+         bytes->hex
+         -with-prefix)
+    (do
+      (log/warn "Value ignored" {:value value})
+      value)))
+
 
 (defn- -decrypt-value
   [user-public-key ci-secret-key value]
-  (cb/decrypt user-public-key
-              ci-secret-key
-              (sb/int->nonce 0)
-              (hex->bytes value)))
+  (if (-decrypt-it? value)
+    (->> value
+         -trim-prefix
+         hex->bytes
+         (cb/decrypt user-public-key
+                     ci-secret-key
+                     (sb/int->nonce 0))
+         (String.)
+         edn/read-string)
+    value))
 
 
-(defn encrypt
-  [user-secret-key ci-public-key config]
+(defn- -postwalk-values
+  [f form]
   (walk/postwalk
    (fn [x]
      (if (map? x)
        (into {}
-             (map
-              (fn [[k v]]
-                [k (-encrypt-value user-secret-key ci-public-key v)]))
+             (map (fn [[k v]]
+                    [k (f v)]))
              x)
        x))
+   form))
+
+
+(defn encrypt
+  [user-secret-key ci-public-key config]
+  (-postwalk-values
+   (fn [v] (-encrypt-value user-secret-key ci-public-key v))
    config))
 
 
 (defn decrypt
   [user-public-key ci-secret-key cipherconfig]
-  (walk/postwalk
-   (fn [x]
-     (if (map? x)
-       (into {}
-             (map
-              (fn [[k v]]
-                [k (String. (-decrypt-value user-public-key ci-secret-key v))]))
-             x)
-       x))
+  (-postwalk-values
+   (fn [v] (-decrypt-value user-public-key ci-secret-key v))
    cipherconfig))
--- test/beetleman/seconfig_test.clj
+++ test/beetleman/seconfig_test.clj
@@ -25,8 +25,40 @@
                           cipherconfig)))))
 
 
+(t/deftest -with-prefix-trim-prefix-test
+  (let [t        "some random text"
+        prefixed (#'sut/-with-prefix t)]
+    (t/is (not= t
+                prefixed))
+    (t/is (= t
+             (#'sut/-trim-prefix prefixed)))))
+
+
 (t/deftest encrypt-decrypt-test
   (t/testing "flat config with string only values"
              (let [config {"key" "sdsdsdssdsd"
                            :foo  "bar"}]
-               (assert-encrypt-decrypt config))))
+               (assert-encrypt-decrypt config)))
+
+  (t/testing "flat config with string and numbers"
+             (let [config {:string          "sdsdsdssdsd"
+                           :number          1
+                           :negative/number -33
+                           :float           0.1212}]
+               (assert-encrypt-decrypt config)))
+
+  
+  (t/testing "not encrypted values are ignored during decryption"
+             (let [config       {:key "value"}
+                   ci           (sut/keypair!)
+                   user         (sut/keypair!)
+                   cipherconfig (sut/encrypt (:secret user)
+                                             (:public ci)
+                                             config)
+                   other-config {:other-key "value"}]
+               (t/is (= (merge config
+                               other-config)
+                        (sut/decrypt (:public user)
+                                     (:secret ci)
+                                     (merge cipherconfig
+                                            other-config)))))))
