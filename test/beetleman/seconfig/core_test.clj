ec6e35637221d201980925c2a8ae532d16946a1b
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2020-05-01 15:54:45 +0200

    add property-based testing

--- test/beetleman/seconfig/core_test.clj
+++ test/beetleman/seconfig/core_test.clj
@@ -1,6 +1,9 @@
 (ns beetleman.seconfig.core-test
   (:require [beetleman.seconfig.core :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [clojure.test.check.clojure-test :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
 
 
 (t/deftest bytes->hex->bytes>hex-test
@@ -46,7 +49,7 @@
                            :float           0.1212}]
                (assert-encrypt-decrypt config)))
 
-  
+
   (t/testing "not encrypted values are ignored during decryption"
              (let [config       {:key "value"}
                    ci           (sut/keypair!)
@@ -61,3 +64,20 @@
                                      (:secret ci)
                                      (merge cipherconfig
                                             other-config)))))))
+
+
+(tc/defspec
+ encrypt-decrypt-test-spec
+ (let [key-gen          (gen/one-of [gen/string
+                                     gen/keyword
+                                     gen/keyword-ns])
+       container-gen-fn (fn [inner-gen]
+                          (gen/map key-gen inner-gen))
+       scalars-gen      (gen/one-of [gen/int
+                                     gen/string
+                                     gen/keyword
+                                     gen/keyword-ns
+                                     gen/boolean])]
+   (prop/for-all
+    [config (gen/map key-gen (gen/recursive-gen container-gen-fn scalars-gen))]
+    (assert-encrypt-decrypt config))))
